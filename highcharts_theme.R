215444f9f8cad506220ea0211817c66e816579a2
Tom Bishop <tomwbish@gmail.com>
2017-08-23 13:56:36 +1000

    added some sauce

--- highcharts_theme.R
+++ highcharts_theme.R
@@ -3,29 +3,29 @@ high_tmbish = function() {
   out = 
     hc_theme(
       chart = list(
-        backgroundColor = list("white"),
+        backgroundColor = "#FDFFFC",
         style = list(
-          fontFamily = "Droid Sans"
+          fontFamily = "Grand Hotel"
         )
       ),
-      colors = list("#414141", "#FF6A5C", "#CCDFC8", "#056571"),
+      colors = list("#FF9F1C", "#2EC4B6", "#FE5F55", "#FE5F55"),
       title = list(
         style = list(fontWeight = "bold", color = "#414141"),
         align = "left"
       ),
       xAxis = list(
-        gridLineColor = "#2196F3",
-        lineColor = "#2196F3",
-        gridLineWidth = 0.5,
-        lineWidth = 0,
+        gridLineColor = "#011627",
+        lineColor = "#011627",
+        gridLineWidth = 0.1,
+        lineWidth = 1,
         title = list(style = list(fontWeight = "bold"))
       ),
       yAxis = list(
-        gridLineColor = "#2196F3",
-        lineColor = "#2196F3",
-        gridLineWidth = 0.5,
-        lineWidth = 0,
-        title = list(style = list(weight = "bold"))
+        gridLineColor = "#011627",
+        lineColor = "#011627",
+        gridLineWidth = 0.1,
+        lineWidth = 1,
+        title = list(style = list(fontWeight = "bold"))
       ),
       subtitle = list(
         style = list(fontStyle = "italic", color ="#414141"), 
@@ -35,8 +35,29 @@ high_tmbish = function() {
         borderWidth = 0,
         shape = "square",
         valueDecimals = 2
+      ),
+      plotOptions = list(
+        line = list(marker = list(symbol = "circle", lineWidth = 2, radius = 5)),
+        spline = list(marker = list(symbol = "circle", lineWidth = 2, radius = 5))
+      ),
+      legend = list(
+        align = "right",
+        # floating = TRUE,
+        layout = "vertical",
+        backgroundColor = "#FFF",
+        shadow = TRUE,
+        title = "Legend",
+        verticalAlign = "middle"
+      ),
+      exporting = list(
+        enabled = TRUE
+      ),
+      credits = list(
+        enabled = TRUE,
+        href = "https://github.com/TMBish/lab_posts/blob/master/highcharts_theme.R",
+        text = "My schmick highcharts theme"
       )
+      
     )
   
 }
-  
